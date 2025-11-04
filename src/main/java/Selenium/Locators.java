4d530201147cfa1548b78b4451c5432d0e14df4e
Shubham Yadav <>
2025-11-04 17:50:13 +0700

    4th November 2025 (Git practice with Readme File Changes)

--- src/main/java/Selenium/Locators.java
+++ src/main/java/Selenium/Locators.java
@@ -1,5 +1,7 @@
 package Selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +12,7 @@ public class Locators {
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://freelance-learn-automation.vercel.app/login");
 		driver.findElement(By.name("email")).sendKeys("y.shub.64@gmail.com");
 
